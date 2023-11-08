487c2811c93e4872578713e68d8907c4d6f4c5ef
Danil_Akulin <77333205+Danil-Akulin@users.noreply.github.com>
2023-11-08 14:10:18 +0200

    added input components

---
+++ Source/TTHK/Controller/MainPlayerController.h
@@ -0,0 +1,30 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MainPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TTHK_API AMainPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void SetPawn(APawn* InPawn) override;
+
+protected:
+	virtual void SetupInputComponent() override;
+
+private:
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void Turn(float Value);
+	void LookUp(float Value);
+	void Jump();
+
+	TSoftObjectPtr<class AMainCharacter> CachedMainCharacter;
+};
---
+++ Source/TTHK/MainCharacter.h
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MainCharacter.generated.h"
+
+UCLASS()
+class TTHK_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	
+	virtual void MoveForward(float Value) {};
+	virtual void MoveRight(float Value) {};
+	virtual void Turn(float Value) {};
+	virtual void LookUp(float Value) {};
+};
